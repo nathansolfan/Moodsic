7a2d6128bdee30cf05596bef21b60c7080b48e9c
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-07 23:19:12 +0100

    created the login() method on the spotifyController that gets the ID, URI and $scopes. Then redirect

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class SpotifyController extends Controller
 {
-    //
+    public function login()
+    {
+        $clientId = env('SPOTIFY_CLIENT_ID');
+        $redirectUri = env('SPOTIFY_REDIRECT_URI');
+        // Scopes: Spotify users using third-party apps that only the information they choose to share will be shared
+        $scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-public';
+
+        return redirect("https://accounts.spotify.com/authorize?client_id={$clientId}&response_type=code&redirect_uri={$redirectUri}&scope={$scopes}");
+    }
 }
