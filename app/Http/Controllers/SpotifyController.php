d3c9fa074bd9ad37a3190171c9ffb8d90d5b56f4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-07 23:31:17 +0100

    update the callback() method with $code where it has the request->input('code')
    then the authorization code for the access token
    with $client = new Client()
    and the $response with $client->post('htpps:...., ['form_params' => ['...'], ])
    
    and the $body with json_decode($response->getBody());
    
    and store the token in a sesion

--- app/Http/Controllers/SpotifyController.php
+++ app/Http/Controllers/SpotifyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class SpotifyController extends Controller
@@ -14,5 +15,29 @@ class SpotifyController extends Controller
         $scopes = 'user-read-private user-read-email playlist-read-private playlist-modify-public';
 
         return redirect("https://accounts.spotify.com/authorize?client_id={$clientId}&response_type=code&redirect_uri={$redirectUri}&scope={$scopes}");
+    }
+
+    public function callback(Request $request)
+    {
+        $code = $request->input('code');
+
+        // Exchange authorization code for access token
+        $client = new Client();
+        $response = $client->post('https://accounts.spotify.com/api/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'redirect_uri' => env('SPOTIFY_REDIRECT_URI'),
+                'client_id' => env('SPOTIFY_CLIENT_ID'),
+                'client_secret' => env('SPOTIFY_CLIENT_SECRET')
+            ],
+        ]);
+
+        $body = json_decode($response->getBody());
+
+        // store access token in sesion for future requests
+        session(['spotify_access_token' => $body->access_token]);
+
+        return redirect()->route('mood.select');
     }
 }
