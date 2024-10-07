c214c4c2bc50cdf4d29cb39ef7600073a560f603
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-07 23:14:58 +0100

    created the login and callback routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\SpotifyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [SpotifyController::class, 'login'])->name('spotify.login');
+Route::get('/callback', [SpotifyController::class, 'callback'])->name('spotify.callback');
