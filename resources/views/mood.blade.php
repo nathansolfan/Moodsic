f25d40f5a23cee91aa0044d81727b6784abece02
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-08 16:01:47 +0100

    installed tailwind

--- resources/views/mood.blade.php
+++ resources/views/mood.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Select Your Mood</title>
+    @vite('resources/css/app.css')
+
 </head>
 <body>
     <h1>Select your Mood</h1>
