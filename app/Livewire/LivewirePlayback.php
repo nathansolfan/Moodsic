9b3e77e40a94d4d61faef7547869fe2313cac84d
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-11 23:02:12 +0100

    updated C:\Users\natha\Desktop\Moodsic\app\Livewire\LivewirePlayback.php with layout methods

--- app/Livewire/LivewirePlayback.php
+++ app/Livewire/LivewirePlayback.php
@@ -6,6 +6,27 @@ use Livewire\Component;
 
 class LivewirePlayback extends Component
 {
+    public $trackname = 'Loading...';
+    public $artistName = 'Loading...';
+    public $isPlaying = false;
+    public $errorMessage = '';
+
+    public function togglePlay()
+    {
+
+    }
+
+    public function loadTrackInfo()
+    {
+
+    }
+
+    public function mount()
+    {
+
+    }
+
+
     public function render()
     {
         return view('livewire.livewire-playback');
