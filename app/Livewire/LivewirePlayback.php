8510cb13575ba5ce8836aa5365fb83ff02ffe10e
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-12 23:24:40 +0100

    updated

--- app/Livewire/LivewirePlayback.php
+++ app/Livewire/LivewirePlayback.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use GuzzleHttp\Client;
 use Livewire\Component;
 
 class LivewirePlayback extends Component
@@ -10,10 +11,39 @@ class LivewirePlayback extends Component
     public $artistName = 'Loading...';
     public $isPlaying = false;
     public $errorMessage = '';
+    public $albumArt = '';
+    public $progress = '00:00';
+    public $duration = '00:00';
+
+    protected $token;
+
+    public function mount()
+    {
+        // fetch access token from session
+        $this->token = session('spotify_webplayback_token');
+
+        if (!$this->token) {
+            $this->errorMessage = 'No access token available. Please log in.';
+        } else {
+            $this->loadTrackInfo();
+        }
+    }
 
     public function togglePlay()
     {
+        if (!$this->token) {
+            $this->errorMessage = 'No access token available.';
+            return;
+        }
 
+        // Call SPOTIFY API to toggle play/pause
+        $client = new Client();
+        $response = $client->put('https://api.spotify.com/v1/me/player/play', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token,
+            ],
+        ]);
+        $this->isPlaying = !$this->isPlaying;
     }
 
     public function loadTrackInfo()
@@ -21,10 +51,7 @@ class LivewirePlayback extends Component
 
     }
 
-    public function mount()
-    {
 
-    }
 
 
     public function render()
