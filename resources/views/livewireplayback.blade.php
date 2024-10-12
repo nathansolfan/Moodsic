d263ffe468e9392ace762c53de958104d679acf5
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-12 23:41:23 +0100

    updates on liveware view

--- resources/views/livewireplayback.blade.php
+++ resources/views/livewireplayback.blade.php
@@ -23,6 +23,25 @@
         <div class="text-red-500 mb-4 text-center">{{ $errorMessage }}</div>
         @endif
 
+        {{-- Track Ifno --}}
+        <div class="text-center mb-6">
+            <img src=" {{ $albumArt }} " alt="Album Art">
+            <h2> {{ $trackName}} </h2>
+            <p> {{ $artistName}} </p>
+        </div>
+
+        {{-- Play btn --}}
+        <div class="text-center mb-4">
+            <button wire:click="togglePlay" class="bg-green-500 text-white font-bold py-2 px-4 rounded hover:bg-green-600 transition duration-200">
+                {{ $isPlaying ? 'Pause' : 'Play'}}
+            </button>
+        </div>
+
+        {{-- Track progress --}}
+        <div id="progress-container" class="text-center mb-4">
+            <span id="current-progress" class="text-sm"> {{ $progress }} </span>
+        </div>
+
     </div>
 
 </body>
