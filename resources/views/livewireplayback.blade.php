3ffc7ed307e7cf19a50e14a985c642eab5c48cbf
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-13 15:40:12 +0100

    updastes

--- resources/views/livewireplayback.blade.php
+++ resources/views/livewireplayback.blade.php
@@ -1,1 +1,20 @@
-<livewire:livewire-playback />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spotify Web Playback (Livewire)</title>
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body>
+    <div>
+        <h1 class="text-3xl font-bold text-center mb-8">Spotify Web Playback (Livewire)</h1>
+        <!-- Embed the Livewire component -->
+        <livewire:livewire-playback />
+    </div>
+
+    @livewireScripts
+</body>
+</html>
