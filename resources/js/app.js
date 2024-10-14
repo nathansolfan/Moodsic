9001f2511314a90a4f6c04c17e670bd2ce8367ca
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-14 16:21:30 +0100

    Registered React Component in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import React from 'react';
 import './bootstrap';
+import { createRoot } from 'react-dom/client';
+import SpotifyPlayer from './components/SpotifyPlayer';
+
+const spotifyElement = document.getElementById('spotify-player');
+if (spotifyElement) {
+    const root = createRoot(spotifyElement);
+    root.render(<SpotifyPlayer />);
+
+}
