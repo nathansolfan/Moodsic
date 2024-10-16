dc96a0a45aedf98bfe7e3f7152c871948199cc52
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-10-16 14:52:57 +0100

    updates

--- resources/views/reactspotify.blade.php
+++ resources/views/reactspotify.blade.php
@@ -9,7 +9,7 @@
     <meta name="spotify-token" content="{{ session('spotify_webplayback_token') }}">
 
     <!-- Load the Spotify Web Playback SDK -->
-    <script src="https://sdk.scdn.co/spotify-player.js"></script>
+    {{-- <script src="https://sdk.scdn.co/spotify-player.js"></script> --}}
 </head>
 <body>
     <div id="spotify-player"></div>
